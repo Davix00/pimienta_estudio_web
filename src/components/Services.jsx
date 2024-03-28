26f145672f0226ae7a8a285f2eb79b25d10ec951
Perla <pyuritzy21@gmail.com>
2024-03-28 15:57:31 -0600

    Terminando services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,9 @@
 import { AiOutlineLine } from "react-icons/ai";
+import image1 from "../assets/services/1.png";
+import image2 from "../assets/services/2.jpg";
+import image3 from "../assets/services/3.jpg";
+import image4 from "../assets/services/4.png";
+import image5 from "../assets/services/5.png";
 
 const Services = () => {
   return (
@@ -11,8 +16,74 @@ const Services = () => {
         <div className="text-primary flex justify-center">
           <AiOutlineLine className="w-full" />
         </div>
+        <div className="flex flex-wrap justify-center mt-4">
+          <div className="flex flex-col items-center lg:mr-20">
+            <div className="rounded-full overflow-hidden h-24 w-24 border-4 flex items-center justify-center">
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={image1}
+                alt="Imagen de servicio"
+              />
+            </div>
+            <p className="font-semibold text-slate-800 mt-2 text-center w-36">
+              Diseño de logotipo e identidad Gráfica
+            </p>
+          </div>
 
-        
+          <div className="flex flex-col items-center mx-4 lg:mr-20">
+            <div className="rounded-full overflow-hidden h-24 w-24 border-4 flex items-center justify-center">
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={image2}
+                alt="Imagen de servicio"
+              />
+            </div>
+            <p className="font-semibold text-slate-800 mt-2 text-center w-36">
+              Impresión Digital y Offset
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center mx-4">
+            <div className="rounded-full overflow-hidden h-24 w-24 border-4 flex items-center justify-center">
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={image3}
+                alt="Imagen de servicio"
+              />
+            </div>
+            <p className="font-semibold text-slate-800 mt-2 text-center w-20">
+            Diseño editorial
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-center mt-4 lg:mt-8 ">
+          <div className="flex flex-col items-center mx-4">
+            <div className="rounded-full overflow-hidden h-24 w-24 border-4 flex items-center justify-center">
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={image4}
+                alt="Imagen de servicio"
+              />
+            </div>
+            <p className="font-semibold text-slate-800 mt-2 text-center w-36">
+            Páginas Web
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center mx-4 lg:ml-20">
+            <div className="rounded-full overflow-hidden h-24 w-24 border-4 flex items-center justify-center">
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={image5}
+                alt="Imagen de servicio"
+              />
+            </div>
+            <p className="font-semibold text-slate-800 mt-2 text-center w-32">
+            Medios publicitarios
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
