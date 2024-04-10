aa76e72734fda85e2e25b3d49bc3cdaecc8703a7
David Galaviz <galavizgdavid899@gmail.com>
2024-04-10 16:24:01 -0600

    mdoficacion de imagenes y rutas

--- imagesPath.jsx
+++ imagesPath.jsx
@@ -1,89 +1,167 @@
-const carouselPath = "../src/assets/carousel/";
+import imgCarrusel1 from "./src/assets/carousel/carousel1.jpg";
+import imgCarrusel2 from "./src/assets/carousel/carousel2.jpg";
+import imgCarrusel3 from "./src/assets/carousel/carousel3.jpg";
+import imgCarrusel4 from "./src/assets/carousel/carousel4.jpg";
+import imgCarrusel5 from "./src/assets/carousel/carousel5.jpg";
+import imgCarrusel6 from "./src/assets/carousel/carousel6.jpg";
+import imgCarrusel7 from "./src/assets/carousel/carousel7.jpg";
+import imgCarrusel8 from "./src/assets/carousel/carousel8.jpg";
+import imgCarrusel9 from "./src/assets/carousel/carousel9.jpg";
+
 export const imagesCarousel = [
-  carouselPath + "carousel1.jpg",
-  carouselPath + "carousel2.jpg",
-  carouselPath + "carousel3.jpg",
-  carouselPath + "carousel4.jpg",
-  carouselPath + "carousel5.jpg",
-  carouselPath + "carousel6.jpg",
-  carouselPath + "carousel7.jpg",
-  carouselPath + "carousel8.jpg",
-  carouselPath + "carousel9.jpg",
+  imgCarrusel1,
+  imgCarrusel2,
+  imgCarrusel3,
+  imgCarrusel4,
+  imgCarrusel5,
+  imgCarrusel6,
+  imgCarrusel7,
+  imgCarrusel8,
+  imgCarrusel9,
 ];
 
-const clientsPath = "../src/assets/clients/";
+import ayl from "./src/assets/clients/A&L.svg";
+import adesu from "./src/assets/clients/ADESU.svg";
+import apehit from "./src/assets/clients/APEHIT.svg";
+import arteaga from "./src/assets/clients/ARTEAGA.svg";
+import cani from "./src/assets/clients/CANI.svg";
+import canito from "./src/assets/clients/CANITO.svg";
+import carolina_arvizu from "./src/assets/clients/CAROLINA ARVIZU.svg";
+import close_up_tv from "./src/assets/clients/CLOSE UP TV.svg";
+import close_up from "./src/assets/clients/CLOSE UP.svg";
+import dr_azor_bravo from "./src/assets/clients/DR AZOR BRAVO.svg";
+import dra_akeena from "./src/assets/clients/DRA AKEENA.svg";
+import el_habanero from "./src/assets/clients/EL HABANERO.svg";
+import el_lenador from "./src/assets/clients/EL LENADOR.svg";
+import elegance from "./src/assets/clients/ELEGANCE.svg";
+import garcia from "./src/assets/clients/GARCIA.svg";
+import grupo_loinaz from "./src/assets/clients/GRUPO LOINAZ.svg";
+import la_luz from "./src/assets/clients/LA LUZ.svg";
+import lily_salazar from "./src/assets/clients/LILY SALAZAR.svg";
+import los_pinos from "./src/assets/clients/LOS PINOS.svg";
+import lucky_pimienta from "./src/assets/clients/LUCKY PIMIENTA.svg";
+import lucky from "./src/assets/clients/LUCKY.svg";
+import lupita_estetica from "./src/assets/clients/LUPITA ESTETICA.svg";
+import maria_reina from "./src/assets/clients/MARIA REINA.svg";
+import matu from "./src/assets/clients/MATU.svg";
+import murrieta from "./src/assets/clients/MURRIETA.svg";
+import papo from "./src/assets/clients/PAPO.svg";
+import pediadent from "./src/assets/clients/PEDIADENT.svg";
+import pimienta from "./src/assets/clients/PIMIENTA.svg";
+import pineda from "./src/assets/clients/PINEDA.svg";
+import prai from "./src/assets/clients/PRai.svg";
+import prevemed from "./src/assets/clients/PREVEMED.svg";
+import puppy_couture from "./src/assets/clients/PUPPY COUTURE.svg";
+import salud_y_vida from "./src/assets/clients/SALUD Y VIDA.svg";
+import santiago from "./src/assets/clients/SANTIAGO.svg";
+import sukury from "./src/assets/clients/SUKURY.svg";
+import terapia_clinia from "./src/assets/clients/TERAPIA CLINIA.svg";
+import tibudent from "./src/assets/clients/TIBUDENT.svg";
+import villamil from "./src/assets/clients/VILLAMIL.svg";
+import vp_planners from "./src/assets/clients/VP PLANNERS.svg";
+import yeimi from "./src/assets/clients/YEIMI.svg";
+import yupii from "./src/assets/clients/YUPII.svg";
+import zaragoza from "./src/assets/clients/ZARAGOZA.svg";
+
 export const imagesClients = [
-  { path: `${clientsPath}A&L.svg` },
-  { path: `${clientsPath}ADESU.svg` },
-  { path: `${clientsPath}APEHIT.svg` },
-  { path: `${clientsPath}ARTEAGA.svg` },
-  { path: `${clientsPath}CANI.svg` },
-  { path: `${clientsPath}CANITO.svg` },
-  { path: `${clientsPath}CAROLINA ARVIZU.svg` },
-  { path: `${clientsPath}CLOSE UP TV.svg` },
-  { path: `${clientsPath}CLOSE UP.svg` },
-  { path: `${clientsPath}DR AZOR BRAVO.svg` },
-  { path: `${clientsPath}DRA AKEENA.svg` },
-  { path: `${clientsPath}EL HABANERO.svg` },
-  { path: `${clientsPath}EL LENADOR.svg` },
-  { path: `${clientsPath}ELEGANCE.svg` },
-  { path: `${clientsPath}GARCIA.svg` },
-  { path: `${clientsPath}GRUPO LOINAZ.svg` },
-  { path: `${clientsPath}LA LUZ.svg` },
-  { path: `${clientsPath}LILY SALAZAR.svg` },
-  { path: `${clientsPath}LOS PINOS.svg` },
-  { path: `${clientsPath}LUCKY PIMIENTA.svg` },
-  { path: `${clientsPath}LUCKY.svg` },
-  { path: `${clientsPath}LUPITA ESTETICA.svg` },
-  { path: `${clientsPath}MARIA REINA.svg` },
-  { path: `${clientsPath}MATU.svg` },
-  { path: `${clientsPath}MURRIETA.svg` },
-  { path: `${clientsPath}PAPO.svg` },
-  { path: `${clientsPath}PEDIADENT.svg` },
-  { path: `${clientsPath}PIMIENTA.svg` },
-  { path: `${clientsPath}PINEDA.svg` },
-  { path: `${clientsPath}PRai.svg` },
-  { path: `${clientsPath}PREVEMED.svg` },
-  { path: `${clientsPath}PUPPY COUTURE.svg` },
-  { path: `${clientsPath}SALUD Y VIDA.svg` },
-  { path: `${clientsPath}SANTIAGO.svg` },
-  { path: `${clientsPath}SUKURY.svg` },
-  { path: `${clientsPath}TERAPIA CLINIA.svg` },
-  { path: `${clientsPath}TIBUDENT.svg` },
-  { path: `${clientsPath}VILLAMIL.svg` },
-  { path: `${clientsPath}VP PLANNERS.svg` },
-  { path: `${clientsPath}YEIMI.svg` },
-  { path: `${clientsPath}YUPII.svg` },
-  { path: `${clientsPath}ZARAGOZA.svg` },
+  { path: ayl },
+  { path: adesu },
+  { path: apehit },
+  { path: arteaga },
+  { path: cani },
+  { path: canito },
+  { path: carolina_arvizu },
+  { path: close_up_tv },
+  { path: close_up },
+  { path: dr_azor_bravo },
+  { path: dra_akeena },
+  { path: el_habanero },
+  { path: el_lenador },
+  { path: elegance },
+  { path: garcia },
+  { path: grupo_loinaz },
+  { path: la_luz },
+  { path: lily_salazar },
+  { path: los_pinos },
+  { path: lucky_pimienta },
+  { path: lucky },
+  { path: lupita_estetica },
+  { path: maria_reina },
+  { path: matu },
+  { path: murrieta },
+  { path: papo },
+  { path: pediadent },
+  { path: pimienta },
+  { path: pineda },
+  { path: prai },
+  { path: prevemed },
+  { path: puppy_couture },
+  { path: salud_y_vida },
+  { path: santiago },
+  { path: sukury },
+  { path: terapia_clinia },
+  { path: tibudent },
+  { path: villamil },
+  { path: vp_planners },
+  { path: yeimi },
+  { path: yupii },
+  { path: zaragoza },
 ];
 
-const colavorationsPath = "../src/assets/colavorations/";
+import adagio from "./src/assets/colavorations/adagio.svg";
+import arquidiocesis from "./src/assets/colavorations/arquidiocesis.svg";
+import carls from "./src/assets/colavorations/carls.svg";
+import ciep from "./src/assets/colavorations/ciep.svg";
+import cisneros from "./src/assets/colavorations/cisneros.svg";
+import cristo_rey from "./src/assets/colavorations/cristo rey.svg";
+import cristobal from "./src/assets/colavorations/cristobal.svg";
+import cuple from "./src/assets/colavorations/cuple.svg";
+import dominicas from "./src/assets/colavorations/dominicas.svg";
+import escudo_arquidiocesis from "./src/assets/colavorations/escudo arquidiocesis.svg";
+import haus from "./src/assets/colavorations/haus.svg";
+import haz_tu_loko from "./src/assets/colavorations/haz tu loko.svg";
+import hscj from "./src/assets/colavorations/hscj.svg";
+import kids_and_us from "./src/assets/colavorations/kids&us.svg";
+import kiwi from "./src/assets/colavorations/kiwi.svg";
+import la_chinata from "./src/assets/colavorations/la chinata.svg";
+import lupemaurer from "./src/assets/colavorations/lupemaurer.svg";
+import niño_docor from "./src/assets/colavorations/niño docor.svg";
+import ogcm from "./src/assets/colavorations/ogcm.svg";
+import pastoral_social from "./src/assets/colavorations/pastoral social.svg";
+import san_hipolito from "./src/assets/colavorations/san hipolito.svg";
+import san_miguel from "./src/assets/colavorations/san miguel.svg";
+import seminairo_menor from "./src/assets/colavorations/seminairo menor.svg";
+import seminario_palafoxiano from "./src/assets/colavorations/seminario palafoxiano.svg";
+import sonata from "./src/assets/colavorations/sonata.svg";
+import transmisiones from "./src/assets/colavorations/transmisiones.svg";
+import vocaciones from "./src/assets/colavorations/vocaciones.svg";
+
 export const imagesColavorations = [
-  { path: `${colavorationsPath}adagio.svg` },
-  { path: `${colavorationsPath}arquidiocesis.svg` },
-  { path: `${colavorationsPath}carls.svg` },
-  { path: `${colavorationsPath}ciep.svg` },
-  { path: `${colavorationsPath}cisneros.svg` },
-  { path: `${colavorationsPath}cristo rey.svg` },
-  { path: `${colavorationsPath}cristobal.svg` },
-  { path: `${colavorationsPath}cuple.svg` },
-  { path: `${colavorationsPath}dominicas.svg` },
-  { path: `${colavorationsPath}escudo arquidiocesis.svg` },
-  { path: `${colavorationsPath}haus.svg` },
-  { path: `${colavorationsPath}haz tu loko.svg` },
-  { path: `${colavorationsPath}hscj.svg` },
-  { path: `${colavorationsPath}kids&us.svg` },
-  { path: `${colavorationsPath}kiwi.svg` },
-  { path: `${colavorationsPath}la chinata.svg` },
-  { path: `${colavorationsPath}lupemaurer.svg` },
-  { path: `${colavorationsPath}niño docor.svg` },
-  { path: `${colavorationsPath}ogcm.svg` },
-  { path: `${colavorationsPath}pastoral social.svg` },
-  { path: `${colavorationsPath}san hipolito.svg` },
-  { path: `${colavorationsPath}san miguel.svg` },
-  { path: `${colavorationsPath}seminairo menor.svg` },
-  { path: `${colavorationsPath}seminario palafoxiano.svg` },
-  { path: `${colavorationsPath}sonata.svg` },
-  { path: `${colavorationsPath}transmisiones.svg` },
-  { path: `${colavorationsPath}vocaciones.svg` },
+  { path: adagio },
+  { path: arquidiocesis },
+  { path: carls },
+  { path: ciep },
+  { path: cisneros },
+  { path: cristo_rey },
+  { path: cristobal },
+  { path: cuple },
+  { path: dominicas },
+  { path: escudo_arquidiocesis },
+  { path: haus },
+  { path: haz_tu_loko },
+  { path: hscj },
+  { path: kids_and_us },
+  { path: kiwi },
+  { path: la_chinata },
+  { path: lupemaurer },
+  { path: niño_docor },
+  { path: ogcm },
+  { path: pastoral_social },
+  { path: san_hipolito },
+  { path: san_miguel },
+  { path: seminairo_menor },
+  { path: seminario_palafoxiano },
+  { path: sonata },
+  { path: transmisiones },
+  { path: vocaciones },
 ];
